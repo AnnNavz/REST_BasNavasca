2df9f15c01dcd25887efb9a43fdb58fd5fa4c5fa
AnnNavz <ccs.annshirleynavasca@gmail.com>
2026-03-12 00:09:55 +0800

    Hatdogg nanaman

--- REST_BasNavasca/MauiProgram.cs
+++ REST_BasNavasca/MauiProgram.cs
@@ -13,7 +13,16 @@ namespace REST_BasNavasca
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+					fonts.AddFont("Outfit-Regular.ttf", "OutfitRegular");
+					fonts.AddFont("Outfit-Semibold.ttf", "OutfitSemibold");
+					fonts.AddFont("Outfit-Black.ttf", "OutfitBlack");
+					fonts.AddFont("Outfit-Bold.ttf", "OutfitBold");
+					fonts.AddFont("Outfit-ExtraBold.ttf", "OutfitExtraBold");
+					fonts.AddFont("Outfit-ExtraLight.ttf", "OutfitExtraLight");
+					fonts.AddFont("Outfit-Light.ttf", "OutfitLight");
+					fonts.AddFont("Outfit-Medium.ttf", "OutfitMedium");
+					fonts.AddFont("Outfit-Thin.ttf", "OutfitThin");
+				});
 
 #if DEBUG
     		builder.Logging.AddDebug();
