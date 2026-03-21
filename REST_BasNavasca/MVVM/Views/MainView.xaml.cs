2320a47d5801942ed06da80d33055b0a30de5794
AnnNavz <ccs.annshirleynavasca@gmail.com>
2026-03-21 20:38:57 +0800

    adjusting the ui of user

--- REST_BasNavasca/MVVM/Views/MainView.xaml.cs
+++ REST_BasNavasca/MVVM/Views/MainView.xaml.cs
@@ -1,5 +1,6 @@
 namespace REST_BasNavasca.MVVM.Views;
 using REST_BasNavasca.MVVM.ViewModels;
+using REST_BasNavasca.MVVM.Models;
 public partial class MainView : ContentPage
 {
 	public MainView()
@@ -16,7 +17,14 @@ public partial class MainView : ContentPage
 
 	private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
 	{
-		await Navigation.PushAsync(new User());
+		var border = sender as Border;
+
+		// The BindingContext of the Border is the specific 'Renters' object for that row
+		if (border?.BindingContext is Renters selectedRenter)
+		{
+			// Pass the object to the User page constructor
+			await Navigation.PushAsync(new User(selectedRenter));
+		}
 	}
 
 	protected override void OnAppearing()
--- REST_BasNavasca/MVVM/Views/User.xaml.cs
+++ REST_BasNavasca/MVVM/Views/User.xaml.cs
@@ -1,10 +1,23 @@
+using REST_BasNavasca.MVVM.Models;
+
 namespace REST_BasNavasca.MVVM.Views;
 
 public partial class User : ContentPage
 {
-	public User()
+	public User(Renters renter)
 	{
 		InitializeComponent();
+
+		if (renter != null)
+		{
+			NameEntry.Text = renter.Name;
+			ContactEntry.Text = renter.Contact;
+			DatePicker.Date = renter.Date;
+			AddressEntry.Text = renter.Address;
+			VehicleEntryData.Text = renter.VehicleModel;
+			UserProfileImage.Source = renter.Profile;
+
+		}
 	}
 
     private void Update_Clicked(object sender, EventArgs e)
@@ -17,5 +30,19 @@ public partial class User : ContentPage
 		VehicleEntryData.IsVisible = false;
 		SubmitUpdate.IsVisible = true;
 		Update.IsVisible = false;
+		CancelUpdate.IsVisible = true;
     }
+
+	private void CancelUpdate_Clicked(object sender, EventArgs e)
+	{
+		NameEntry.IsEnabled = false;
+		ContactEntry.IsEnabled = false;
+		DatePicker.IsEnabled = false;
+		AddressEntry.IsEnabled = false;
+		VehiclePicker.IsVisible = false;
+		VehicleEntryData.IsVisible = true;
+		SubmitUpdate.IsVisible = false;
+		Update.IsVisible = true;
+		CancelUpdate.IsVisible = false;
+	}
 }
